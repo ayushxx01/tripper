d9d68bb049fdca2f9b348b6e1889581f9b7be2de
ayushxx01 <ayushxx01s@gmail.com>
2026-09-19 23:49:45 +0530

    feat: added KMeans clustering for travel coordinates, working fine

--- clustering/app.py
+++ clustering/app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from sklearn.cluster import KMeans
 
 app = Flask(__name__)
 
 @app.route('/cluster', methods=['POST'])
 def cluster():
-    return "Hello"
+    data = request.get_json()
+    points = data['points']
+    days = data['days']
+
+    kmeans = KMeans(n_clusters=days)
+    labels = kmeans.fit_predict(points)
+    
+    grouped = {}
+    for point, label in zip(points,labels):
+        label = int(label)
+        if label not in grouped:
+            grouped[label]=[]
+            
+        grouped[label].append(point)
+    
+    return jsonify(grouped)
+
 
 if __name__ == '__main__':
     app.run(port=5001)
